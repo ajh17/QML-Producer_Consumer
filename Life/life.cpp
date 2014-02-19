646c9787ed3f3f634f4bb42308398c338f6e618d
Akshay Hegde <akshay.hegde17@gmail.com>
2014-02-19 11:33:08 -0800

    Add cpp files

---
+++ Life/life.cpp
@@ -0,0 +1,6 @@
+#include "life.h"
+
+Life::Life(QObject *parent) :
+    QObject(parent)
+{
+}
---
+++ Life/life.h
@@ -0,0 +1,18 @@
+#ifndef LIFE_H
+#define LIFE_H
+
+#include <QObject>
+
+class Life : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Life(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // LIFE_H
