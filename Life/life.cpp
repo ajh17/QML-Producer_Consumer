69678c1c26e2089043142f995f6e0a4f3fe5a51d
Akshay Hegde <akshay.hegde17@gmail.com>
2014-02-19 11:36:47 -0800

    Reinitialize git repo

--- Life/life.cpp
+++ Life/life.cpp
@@ -3,4 +3,18 @@
 Life::Life(QObject *parent) :
     QObject(parent)
 {
+    m_color = "white";
+}
+
+void Life::setBackgroundColor(const QColor &c)
+{
+    if (c != m_color) {
+        m_color = c;
+        emit backgroundColorChanged();
+    }
+}
+
+QColor Life::backgroundColor() const
+{
+    return m_color;
 }
---
+++ build-Life-Desktop-Debug/Life.app/Contents/Resources/qml/Life/Box.qml
@@ -0,0 +1,12 @@
+import QtQuick 2.0
+
+Rectangle {
+    width: 20; height: 20
+    color: "#fff"
+    border.color: "#000"
+    border.width: 1
+    MouseArea {
+        anchors.fill: parent
+        // onClicked:
+    }
+}
