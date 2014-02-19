78cf2f14977a7bc069fc84452146b66fdc620fa5
Akshay Hegde <akshay.hegde17@gmail.com>
2014-02-19 12:22:47 -0800

    Complete message to qml from c++ and add producer class.

--- Life/main.cpp
+++ Life/main.cpp
@@ -1,13 +1,33 @@
 #include <QtGui/QGuiApplication>
+#include <QQmlContext>
 #include "qtquick2applicationviewer.h"
+#include "life.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    QQuickView viewer;
+    Life life;
+    QVariant returnVal;
+    QVariant message = "Hello from C++";
 
-    QtQuick2ApplicationViewer viewer;
-    viewer.setMainQmlFile(QStringLiteral("qml/Life/main.qml"));
-    viewer.showExpanded();
+    viewer.rootContext()->setContextProperty("_life", &life);
+    viewer.setSource(QUrl::fromLocalFile("../../../../Life/qml/Life/main.qml"));
+    viewer.setTitle("Life");
+    viewer.setHeight(120);
+    viewer.setWidth(120);
 
+    QObject *object = viewer.rootObject()->findChild<QObject*>("box1");
+    QObject *root = viewer.rootObject();
+    QMetaObject::invokeMethod(root, "test", Q_RETURN_ARG(QVariant, returnVal),
+                                            Q_ARG(QVariant, message));
+    if (object) {
+        object->setProperty("color", "red");
+    }
+    else {
+        qDebug() << "Object not found";
+    }
+
+    viewer.show();
     return app.exec();
 }
--- build-Life-Desktop-Debug/Life.app/Contents/Resources/qml/Life/main.qml
+++ build-Life-Desktop-Debug/Life.app/Contents/Resources/qml/Life/main.qml
@@ -1,16 +1,36 @@
 import QtQuick 2.0
 
 Item {
-    function test() {
-        console.log("Got msg: ");
+    id: grid
+    color: "black";
+    function test(msg) {
+        console.log("Got msg: ", msg);
+        grid.color = "white";
         return 0;
     }
 
-    id: grid
-    Box { objectName: "testing"; id: box1; x: 0; y: 0 }
-    Box { id: box2; x: 20; y: 0 }
-    Box { id: box3; x: 40; y: 0 }
-    Box { id: box4; x: 60; y: 0 }
-    Box { id: box5; x: 80; y: 0 }
-    Box { id: box6; x: 100; y: 0 }
+    Box {
+        objectName: "box1"
+        x: 0; y: 0
+    }
+    Box {
+        objectName: "box2"
+        x: 20; y: 0
+    }
+    Box {
+        objectName: "box3"
+        x: 40; y: 0
+    }
+    Box {
+        objectName: "box4"
+        x: 60; y: 0
+    }
+    Box {
+        objectName: "box5"
+        x: 80; y: 0
+    }
+    Box {
+        objectName: "box6"
+        x: 100; y: 0
+    }
 }
