4c989bc98a8741a2210f0d694859960e5e29213d
Akshay Hegde <akshay.hegde17@gmail.com>
2014-02-19 18:44:20 -0800

    Simplify helper script

--- Life/helper.js
+++ Life/helper.js
@@ -5,33 +5,17 @@ function createBox() {
     component = Qt.createComponent("qml/Life/Box.qml");
     console.log(component.errorString());
     if (component.status === Component.Ready) {
-        finishCreation();
+        box = component.createObject(appWindow, { "x": xVal, "y": yVal });
+        console.log("Creating box at (" + xVal + ", " + yVal + ")");
+        return box;
     }
     else {
-        component.statusChanged.connect(finishCreation);
-    }
-}
-
-function finishCreation() {
-    var xVal = Math.floor((Math.random() * 500) + 1);
-    var yVal = Math.floor((Math.random() * 500) + 1);
-
-    if (component.status === Component.Ready) {
-        box = component.createObject(appWindow, { "x": xVal, "y": yVal });
-        if (box === null) {
-            console.log("Error creating the box");
-        }
-        else {
-            console.log("Creating box at (" + xVal + ", " + yVal + ")");
-        }
-    }
-    else if (component.status === Component.Error) {
-        console.log("Error loading the box:", component.errorString());
+        console.log("Something broke.");
     }
 }
 
 function destroyRect() {
-    var itemID = "rectID";
-    console.log("Destroying box at (" + itemID.x + ", " + itemID.y + ")");
-    itemID.destroy();
+    if (!box) {
+        box.destroy();
+    }
 }
