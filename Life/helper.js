bb9063cee1310a6eb5da58ac1c6ede610279e99a
Akshay Hegde <akshay.hegde17@gmail.com>
2014-02-24 12:23:00 -0800

    Fix xVal and yVal values from not being generated

--- Life/helper.js
+++ Life/helper.js
@@ -3,10 +3,13 @@ var box;
 
 function createBox() {
     component = Qt.createComponent("qml/Life/Box.qml");
-    console.log(component.errorString());
+    var xVal = Math.floor((Math.random() * 500) + 1);
+    var yVal = Math.floor((Math.random() * 500) + 1);
+
     if (component.status === Component.Ready) {
         box = component.createObject(appWindow, { "x": xVal, "y": yVal });
         console.log("Creating box at (" + xVal + ", " + yVal + ")");
+        console.log(box);
         return box;
     }
     else {
