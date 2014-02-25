dbca99a04a7edf98f77de13b8d3ca0a1abb0080e
Akshay Hegde <akshay.hegde17@gmail.com>
2014-02-24 16:12:53 -0800

    Refactor helper.js and make box and component objects local

--- Life/helper.js
+++ Life/helper.js
@@ -1,37 +1,25 @@
-var component;
-var box;
-
 function createBox() {
-    component = Qt.createComponent("qml/Life/Box.qml");
+    var component = Qt.createComponent("qml/Life/Box.qml");
     var xVal = Math.floor((Math.random() * 500) + 1);
     var yVal = Math.floor((Math.random() * 500) + 1);
 
     if (component.status === Component.Ready) {
-        finishCreation();
-    }
-    else {
-        component.statusChanged.connect(finishCreation);
-    }
-}
-
-function finishCreation() {
-    var xVal = Math.floor((Math.random() * 500) + 1);
-    var yVal = Math.floor((Math.random() * 500) + 1);
-
-    if (component.status === Component.Ready) {
-        box = component.createObject(appWindow, { "x": xVal, "y": yVal });
+        var box = component.createObject(appWindow, { "x": xVal, "y": yVal });
         if (box === null) {
             console.log("Error creating the box");
         }
         else {
-            console.log("Creating" + box + "(" + xVal + ", " + yVal + ")");
+            console.log("Creating " + box + "(" + xVal + ", " + yVal + ")");
+            return box;
         }
+        return 0;
     }
     else if (component.status === Component.Error) {
         console.log("Error loading the box:", component.errorString());
     }
 }
 
+// FIXME: pass the hash item stored in producer.
 function destroyRect() {
     var itemID = "rectID";
     console.log("Destroying box at (" + itemID.x + ", " + itemID.y + ")");
