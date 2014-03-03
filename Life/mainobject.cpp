bcc0669a3415698ffcf553343e1c0c9c29cdd48b
Akshay Hegde <akshay.hegde17@gmail.com>
2014-03-03 14:21:52 -0800

    Implement QHash inside MainObject instead of the Producer.

--- Life/mainobject.cpp
+++ Life/mainobject.cpp
@@ -1,6 +1,23 @@
 #include "mainobject.h"
 
-MainObject::MainObject(QObject *parent) :
-    QObject(parent)
+MainObject::MainObject(QObject* obj, QObject *parent) : QObject(parent)
 {
+    m_obj = obj;
+    id = 1;
+}
+
+void MainObject::insertBox(QVariant box)
+{
+    boxHash.insert(id, box);
+    id += 1;
+}
+
+QVariant MainObject::getBox(int id)
+{
+    return boxHash.value(id);
+}
+
+int MainObject::hashSize()
+{
+    return boxHash.size();
 }
--- Life/mainobject.h
+++ Life/mainobject.h
@@ -1,18 +1,21 @@
 #ifndef MAINOBJECT_H
 #define MAINOBJECT_H
-
 #include <QObject>
+#include <QVariant>
+#include <QHash>
 
 class MainObject : public QObject
 {
     Q_OBJECT
 public:
-    explicit MainObject(QObject *parent = 0);
-
-signals:
-
+    explicit MainObject(QObject* obj = 0, QObject *parent = 0);
+    QHash<int, QVariant> boxHash;
 public slots:
-
+    void insertBox(QVariant box);
+    int hashSize();
+    QVariant getBox(int id);
+private:
+    QObject *m_obj;
+    int id;
 };
-
-#endif // MAINOBJECT_H
+#endif
