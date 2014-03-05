23047d42a9dce1cafc033dc27d3dd3f0b09f731e
Akshay Hegde <akshay.hegde17@gmail.com>
2014-03-05 14:12:14 -0800

    Set up two threads for Producer and Consumer.

--- Life/main.cpp
+++ Life/main.cpp
@@ -10,19 +10,28 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-    QQuickView viewer;
-    QVariant returnVal;
+    qDebug() << "GUI thread reporting in! " << app.thread()->currentThreadId();
 
+    QThread *produceThread = new QThread();
+    QThread *consumeThread = new QThread();
+
+    // Read QML File
+    QQuickView viewer;
     viewer.setSource(QUrl::fromLocalFile("../../../../Life/qml/Life/main.qml"));
+
     QObject *item = viewer.rootObject();
     MainObject *mainObject = new MainObject(item);
     QObject::connect((QObject*)viewer.engine(), SIGNAL(quit()), &app, SLOT(quit()));
 
-    // Start producer's thread
-    qDebug() << "GUI thread reporting in! " << app.thread()->currentThreadId();
+    // Start producer thread
     Producer *producer = new Producer(item, mainObject);
+    producer->moveToThread(produceThread);
+    produceThread->start();
 
-    Consumer consumer(item, mainObject);
+    // Start consumer thread
+    Consumer *consumer = new Consumer(item, mainObject);
+    consumer->moveToThread(consumeThread);
+    consumeThread->start();
 
     viewer.setTitle("Life");
     viewer.setHeight(500);
