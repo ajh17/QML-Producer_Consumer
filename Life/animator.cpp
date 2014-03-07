25bd331aaf9197a1e983ecfa01f88be31d70e564
Akshay Hegde <akshay.hegde17@gmail.com>
2014-03-07 14:12:57 -0800

    Preliminary methods for Animator class

--- Life/animator.cpp
+++ Life/animator.cpp
@@ -1,6 +1,27 @@
 #include "animator.h"
 
-Animator::Animator(QObject *parent) :
-    QObject(parent)
+Animator::Animator(QObject *obj, MainObject *main, QObject *parent) : QObject(parent)
 {
+    m_obj = obj;
+    m_main = main;
+    m_currentBox = 0;
+}
+
+void storeBox(QVariant box)
+{
+    //qDebug() << box;
+    // m_currentBox = box;
+}
+
+void Animator::moveBox()
+{
+    QVariant box;
+    QVariant movedBox;
+
+    int key = m_main->getKeyFor(box);
+
+    // Update the hash
+    QMetaObject::invokeMethod(m_obj, "moveBox", Q_RETURN_ARG(QVariant, box),
+                              Q_ARG(QVariant, movedBox));
+    m_main->updateHash(key, movedBox);
 }
--- Life/animator.h
+++ Life/animator.h
@@ -1,18 +1,19 @@
 #ifndef ANIMATOR_H
 #define ANIMATOR_H
-
-#include <QObject>
+#include "mainobject.h"
 
 class Animator : public QObject
 {
     Q_OBJECT
 public:
-    explicit Animator(QObject *parent = 0);
-
-signals:
-
+    explicit Animator(QObject *obj = 0, MainObject *main = 0, QObject *parent = 0);
 public slots:
-
+    void moveBox();
+    void storeBox(QVariant box);
+private:
+    QObject* m_obj;
+    MainObject* m_main;
+    QVariant m_currentBox;
 };
 
-#endif // ANIMATOR_H
+#endif
