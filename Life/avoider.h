008e42827e61d6345f9e3edf8f6fddc0e3c81bd3
Akshay Hegde <akshay.hegde17@gmail.com>
2014-03-12 15:52:06 -0700

    Rename class.

--- Life/avoider.h
+++ Life/avoider.h
@@ -1,5 +1,8 @@
 #ifndef ANIMATOR_H
 #define ANIMATOR_H
+#include <QTimer>
+#include <QThread>
+#include <QMutex>
 #include "consumer.h"
 
 /* For now, really basic */
@@ -9,6 +12,7 @@ class Animator : public QObject
 public:
     explicit Animator(QObject *obj = 0, Consumer *consumer = 0, MainObject *main = 0,
                       QObject *parent = 0);
+    QTimer *timer;
 public slots:
     /**
      * Checks for collisions and destroys two objects if they collide.
@@ -18,6 +22,7 @@ private:
     QObject *m_obj;
     MainObject *m_main;
     Consumer* m_consumer;
+    QMutex mutex;
 };
 
 #endif
