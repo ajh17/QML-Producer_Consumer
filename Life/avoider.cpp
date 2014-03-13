dba11282cf65b3528e6967f5f9a82a2a905d76ca
Akshay Hegde <akshay.hegde17@gmail.com>
2014-03-12 18:40:50 -0700

    Modify Avoider to call the Consumer to delete collided box.

--- Life/avoider.cpp
+++ Life/avoider.cpp
@@ -1,7 +1,7 @@
 #include "avoider.h"
 
-Avoider::Avoider(QObject *obj, Consumer *consumer, MainObject *main,
-                   QObject *parent) : QObject(parent)
+Avoider::Avoider(QObject *obj, MainObject *main, Consumer *consumer,
+                 QObject *parent) : QObject(parent)
 {
     timer = new QTimer(this);
     m_obj = obj;
@@ -15,17 +15,40 @@ Avoider::Avoider(QObject *obj, Consumer *consumer, MainObject *main,
 void Avoider::checkForCollision()
 {
     QList<QObject *> boxes = m_obj->findChildren<QObject *>("box");
-    QList<QObject *> boxesCopy = m_obj->findChildren<QObject *>("box");
     QList<QObject *>::iterator itr;
 
     // Algorithm is currently O(n^2)
     // TODO: Make this better.
-    foreach(QObject * b, boxesCopy) {
+    foreach(QObject *b, boxes) {
+        QVariant temp1 = QQmlProperty::read(b, "x");
+        QVariant temp2 = QQmlProperty::read(b, "y");
+        double bx = temp1.toDouble();
+        double by = temp2.toDouble();
+
         for (itr = boxes.begin(); itr != boxes.end(); ++itr) {
             if (*itr != b) {
-                // Check bounds of the box comparing it to every other box.
+                QVariant temp3 = QQmlProperty::read(*itr, "x");
+                QVariant temp4 = QQmlProperty::read(*itr, "y");
+                double ix = temp3.toDouble();
+                double iy = temp4.toDouble();
 
+                if ((abs(ix - bx) <= 50) || (abs(iy - by) <= 50)) {
+                    QVariant returnVal;
+                    QVariant box = QVariant::fromValue(b);
+                    QVariant box2 = QVariant::fromValue(*itr);
+                    int id = m_main->getKeyFor(box);
+                    int id2 = m_main->getKeyFor(box2);
+
+                    boxes.removeOne(b);
+                    boxes.removeOne(*itr);
+
+                    qDebug() << "Deleting " << box << "ID: " << id;
+                    m_consumer->consume(id);
+                    qDebug() << "Deleting " << box2 << "ID: " << id2;
+                    m_consumer->consume(id2);
+                }
             }
         }
     }
 }
+
