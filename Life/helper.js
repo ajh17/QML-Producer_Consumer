0459946e642d2c3d9ec6a4aa1aaf4a88f0367112
Akshay Hegde <akshay.hegde17@gmail.com>
2014-03-26 16:00:18 -0700

    Refactor helper.js

--- Life/helper.js
+++ Life/helper.js
@@ -1,23 +1,21 @@
 function createBox() {
-    var component = Qt.createComponent("qml/Life/Box.qml");
-    var xVal = Math.floor((Math.random() * 420) + 1);
-    var yVal = Math.floor((Math.random() * 420) + 1);
-    var box;
+    var component = Qt.createComponent("qml/Life/Box.qml"),
+        xVal      = Math.floor((Math.random() * 420) + 1),
+        yVal      = Math.floor((Math.random() * 420) + 1),
+        box;
 
     if (component.status === Component.Ready) {
         box = component.createObject(appWindow, { "x": xVal, "y": yVal });
         if (box === null) {
             console.log("Error creating the box");
+            return 1;
         }
-        else {
-            console.log("Created " + box + "(" + xVal + ", " + yVal + ")");
-            return box;
-        }
-        return 0;
+        console.log("Created " + box + "(" + xVal + ", " + yVal + ")");
+        return box;
     }
     else if (component.status === Component.Error) {
         console.log("Error loading the box:", component.errorString());
-        return 0;
+        return 2;
     }
 }
 
@@ -28,26 +26,28 @@ function destroyItem(itemID) {
 
 // Destroys boxes on contact.
 function destroyUponCollision(parentID) {
-    var childrenList = parentID.children,
-    var logString = "";
-    var childrenList = parentID.children;
+    var logString    = "",
+        childrenList = parentID.children,
+        rect         = childrenList[0];
+
     for (var i in childrenList) {
         for (var j in childrenList) {
             var firstBox = childrenList[i], secondBox = childrenList[j];
-            if (firstBox !== secondBox) {
-                if (firstBox !== rect && secondBox !== rect) {
-                    var ix = firstBox.x, iy = firstBox.y,
-                    jx = secondBox.x, jy = secondBox.y;
+            if (firstBox !== secondBox && (firstBox !== rect && secondBox !== rect)) {
+                var ix = firstBox.x,
+                    iy = firstBox.y,
+                    jx = secondBox.x,
+                    jy = secondBox.y;
 
-                    if ((Math.abs(ix - jx) <= 50) && (Math.abs(iy - jy) <= 50)) {
-                        logString += "<<***>> " + firstBox + " (" +  firstBox.x + "," + firstBox.y;
-                        logString += ") and " + secondBox + " (" + firstBox.x + "," , + firstBox.y;
-                        logString += ") have collided.\n";
-                        console.log(logString);
+                if (Math.abs(ix - jx) <= 50 && Math.abs(iy - jy) <= 50) {
+                    logString += "<<***>> " + firstBox + " (" +  firstBox.x;
+                    logString += "," + firstBox.y;
+                    logString += ") and " + secondBox + " (" + firstBox.x;
+                    logString += "," + firstBox.y + ") have collided.\n";
+                    console.log(logString);
 
-                        destroyItem(firstBox);
-                        destroyItem(secondBox);
-                    }
+                    parentID.consumeSignal(firstBox);
+                    parentID.consumeSignal(secondBox);
                 }
             }
         }
@@ -56,10 +56,8 @@ function destroyUponCollision(parentID) {
 
 function getNewVal(oldVal) {
     var newVal = Math.floor((Math.random() * 420) + 1);
-    if (newVal === oldVal) {
-        getNewVal(oldVal);
-    }
-    else {
+    if (newVal !== oldVal) {
         return newVal;
     }
+    return getNewVal(oldVal);
 }
