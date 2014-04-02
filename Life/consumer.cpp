5f42452e51b55e7a2d406f8d9b6a8de7131c9fda
Akshay Hegde <akshay.hegde17@gmail.com>
2014-04-02 13:30:23 -0700

    Remove viewer dependency from Consumer. Setup signals

--- Life/consumer.cpp
+++ Life/consumer.cpp
@@ -1,10 +1,8 @@
 #include "consumer.h"
 
-Consumer::Consumer(QObject *obj, MainObject *main,
-                   QObject *parent) : QObject(parent)
+Consumer::Consumer(MainObject *main, QObject *parent) : QObject(parent)
 {
     timer = new QTimer(this);
-    m_obj = obj;
     m_main = main;
     connect(timer, SIGNAL(timeout()), this, SLOT(startConsuming()));
     timer->start(3000);
@@ -12,19 +10,15 @@ Consumer::Consumer(QObject *obj, MainObject *main,
 
 void Consumer::startConsuming()
 {
-    if (!m_obj) {
-        qDebug() << "Viewer was null.";
-        return;
-    }
-    m_threadID = this->thread()->currentThreadId();
-    qDebug() << "Consumer Thread ID: " << m_threadID;
-
     QList<int> keyList = m_main->getKeys();
     int key_size = keyList.size();
+
+    qDebug() << "Consumer Thread ID: " << m_threadID;
+    m_threadID = this->thread()->currentThreadId();
+
     if (key_size != 0) {
         qDebug() << key_size;
         int randomID = qrand() % key_size;
-
         this->consume(keyList[randomID], m_threadID , false);
     }
 }
@@ -40,25 +34,15 @@ void Consumer::consume(int id, Qt::HANDLE threadID, bool collision)
     if (id == 0 || m_main->getBox(id).isNull()) {
         return;
     }
-    qDebug() << "ID: " << id;
 
-    QObject *collisionTextBox, *consumerTextBox;
+    qDebug() << "ID: " << id;
+    QVariant box = m_main->removeBox(id);
     if (collision) {
-        collisionTextBox = m_obj->findChild<QObject *>("collisionTextBox");
-        if (collisionTextBox) {
-            collisionTextBox->setProperty("color", "red");
-        }
+        emit collisionSignal(box);
     }
     else {
-        consumerTextBox = m_obj->findChild<QObject *>("consumerTextBox");
-        if (consumerTextBox) {
-            consumerTextBox->setProperty("color", "red");
-        }
+        emit consumedSignal(box);
     }
-
-    QVariant box = m_main->removeBox(id);
-    QMetaObject::invokeMethod(m_obj, "destroyBox", Qt::QueuedConnection,
-                              Q_ARG(QVariant, box));
 }
 
 void Consumer::consumeSlot(const QVariant &obj)
--- Life/consumer.h
+++ Life/consumer.h
@@ -8,14 +8,16 @@ class Consumer : public QObject
 {
     Q_OBJECT
 public:
-    explicit Consumer(QObject *obj = 0, MainObject *main = 0, QObject *parent = 0);
+    explicit Consumer(MainObject *main = 0, QObject *parent = 0);
     QTimer *timer;
 public slots:
     void startConsuming();
     void consume(int id, Qt::HANDLE threadID, bool collision);
     Q_INVOKABLE void consumeSlot(const QVariant &obj);
+signals:
+    void collisionSignal(QVariant &box);
+    void consumedSignal(QVariant &box);
 private:
-    QObject *m_obj;
     Qt::HANDLE m_threadID;
     MainObject *m_main;
 };
